4fd58f12b655cb5ae87dc65fc41f9ec2a2d1431d
deer5000 <khidrbrinkley32@gmail.com>
2023-04-25 16:59:08 -0400

    added math functions

--- Algorithms/palindromeNumber.playground/Contents.swift
+++ Algorithms/palindromeNumber.playground/Contents.swift
@@ -1,5 +1,45 @@
 import UIKit
 
+
+func addTwoNums(num1: Int, num2: Int) ->  Int {
+    return num1 + num2
+}
+addTwoNums(num1: 15, num2: 25)
+
+
+func subtractTwoNums(num1: Int, num2: Int) -> Int {
+    return num1 - num2
+}
+subtractTwoNums(num1: 50, num2: 35)
+
+
+func divTwoNums(num1: Int , num2: Int) -> Int {
+    return num1 / num2
+}
+divTwoNums(num1: 100, num2: 4)
+
+func timesTwonums(num1: Int, num2: Int) -> Int {
+    return num1 * num2
+}
+timesTwonums(num1: 10000, num2: 80)
+
+
+func doubleNum(num1: Double, num2: Double) -> Double {
+    return num1 * num2
+}
+doubleNum(num1: 20.5, num2: 90.6)
+
+func isWordlengthGreaterThanNum(word: String, num: Int) -> Bool {
+    if word.count > num {
+        return true
+    } else {
+        return false
+    }
+}
+isWordlengthGreaterThanNum(word: "Hello World", num: 20)
+
+
+//##################################################
 func isPalindrome(_ x: Int) -> Bool {
     // convert x(integer) into a string so that we can iterate through it
     var num2String = ("\(x)")
@@ -22,5 +62,7 @@ func isPalindrome(_ x: Int) -> Bool {
 
 //function call
 isPalindrome(10)
+
+//##########################################
 
 
